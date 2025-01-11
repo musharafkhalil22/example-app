4128faa253bbdc24f3b60fb3061b726f11b20117
musharafkhalil22 <musharafkhalil2000@gmail.com>
2025-01-11 15:14:16 +0500

    loginUserController

--- app/Http/Controllers/Auth/LoginUserController.php
+++ app/Http/Controllers/Auth/LoginUserController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class RegisteredUserController extends Controller
+class LoginUserController extends Controller
 {
     //
 }
