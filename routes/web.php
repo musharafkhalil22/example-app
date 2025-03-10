b9e0626507b65d5d5264a6f94d7b24ac6051e124
musharafkhalil22 <musharafkhalil2000@gmail.com>
2025-03-10 08:19:04 +0500

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\LoginUserController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Auth
+
+Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
+
+Route::Post('register', [RegisteredUserController::class, 'store']);
+
+Route::get('login', [LoginUserController::class, 'create'])->name('login');
+
+Route::Post('login', [LoginUserController::class, 'store']);
+
+Route::Post('logout', [LoginUserController::class, 'destroy'])->name('logout');
